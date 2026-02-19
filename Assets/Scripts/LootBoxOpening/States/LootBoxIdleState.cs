df8d6edb757f120eaa6c2a9fae93a7d8f0c85411
Pimax <perko.lesha@gmail.com>
2026-02-19 14:02:47 +0300

    Folders organizing

---
+++ Assets/Scripts/LootBoxOpening/States/LootBoxIdleState.cs
@@ -0,0 +1,41 @@
+﻿using AxGrid.FSM;
+
+namespace LootBoxOpening.States
+{
+    [State("LootBoxIdleState")]
+    public class LootBoxIdleState : FSMState
+    {
+        private bool _isStartRequested;
+
+        [Enter]
+        private void Enter()
+        {
+            _isStartRequested = false;
+
+            Model.Set("BtnLootBoxStartButtonEnable", true);
+            Model.Set("BtnLootBoxStopButtonEnable", false);
+
+            Model.EventManager.AddAction("OnStartLootBoxScroll", OnStartCmd);
+        }
+
+        private void OnStartCmd()
+        {
+            _isStartRequested = true;
+        }
+
+        [Loop(0f)]
+        private void Tick(float delta)
+        {
+            if (!_isStartRequested) return;
+
+            _isStartRequested = false;
+            Parent.Change("LootBoxScrollingState");
+        }
+
+        [Exit]
+        private void Exit()
+        {
+            Model.EventManager.RemoveAction("OnStartLootBoxScroll", OnStartCmd);
+        }
+    }
+}
---
+++ Assets/Scripts/LootBoxOpening/States/LootBoxStoppingState.cs
@@ -0,0 +1,41 @@
+﻿using AxGrid.FSM;
+
+namespace LootBoxOpening.States
+{
+    [State("LootBoxStoppingState")]
+    public class LootBoxStoppingState : FSMState
+    {
+        private bool _isAnimationFinished;
+
+        [Enter]
+        private void Enter()
+        {
+            _isAnimationFinished = false;
+
+            Model.Set("BtnLootBoxStopButtonEnable", false);
+            Model.EventManager.Invoke("StopVisualScroll");
+            Model.EventManager.AddAction("OnAnimationFinished", OnFinished);
+        }
+
+        private void OnFinished()
+        {
+            _isAnimationFinished = true;
+        }
+
+        [Loop(0f)]
+        private void Tick(float delta)
+        {
+            if (!_isAnimationFinished) return;
+
+            _isAnimationFinished = false;
+
+            Parent.Change("LootBoxIdleState");
+        }
+
+        [Exit]
+        private void Exit()
+        {
+            Model.EventManager.RemoveAction("OnAnimationFinished", OnFinished);
+        }
+    }
+}
